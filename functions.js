ae02bd86e3c645a32c1324a0379918a7654722eb
Andrew Gomez <ootbyc@gmail.com>
2018-11-06 13:15:29 -0500

    Buttons

---
+++ functions.js
@@ -0,0 +1,17 @@
+function myconsole() {
+  console.log("Hello, World!");
+}
+
+function popup() {
+  alert("Hello, World!");
+}
+
+function text() {
+  var p=document.getElementById("appear");
+  p.innerHTML="Hello, World";
+}
+
+function stranger() {
+  var name=prompt("Enter your name")
+  alert("Hello " + name)
+}
